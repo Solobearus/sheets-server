394d355bcea21f18449d3afb54edd777941aed0e
solobearus <ivansolobear@gmail.com>
2020-03-09 21:57:24 +0200

    now works with sockets

--- app.js
+++ app.js
@@ -2,19 +2,57 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var sheet = require('./data/data.json');
+var cors = require('cors')
 
 var app = express();
+var server = require(`http`).Server(app);
+var io = require(`socket.io`)(server);
+
+app.use(function (req, res, next) {
+    res.io = io;
+    next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/api/Sheet/Get', (req, res) => {
 
-module.exports = app;
+    console.log('/api/Sheet/Get works');
+    
+    if (!sheet) {
+        res.status(400).json({ err: err });
+    } else {
+        res.status(200).json(sheet);
+    }
+})
+
+app.post('/api/Sheet/Save', (req, res) => {
+
+    const { row, col, text } = req.body;
+
+    // console.log('test');
+    // console.log(sheet);
+    // console.log("req.body", req.body);
+
+    if (!sheet) {
+        res.status(400);
+    } else {
+        if (sheet.Cells[row]) {
+            sheet.Cells[row][col] = text;
+        } else {
+            sheet.Cells[row] = { [col]: text };
+        }
+
+        io.emit('onCellSave', sheet.Cells);
+        res.status(200).json({ res: sheet.Cells });
+
+    }
+});
+
+module.exports = {app:app, server:server};
